b577fe057568e80b5ff330ab55a593f93867962e
Денис Григорьев <Denisgri25@outlook.com>
2021-04-16 13:50:00 +0300

    add prime numbers list

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ namespace RSA_ElGamal
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("List of prime numbers:\n" +
+                              "2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,\n" +
+                              "101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197,\n" +
+                              "199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271\n");
             Console.WriteLine("Write prime number p:");
             long p = long.Parse(Console.ReadLine() ?? string.Empty);
             Console.WriteLine("Write prime number q:");
